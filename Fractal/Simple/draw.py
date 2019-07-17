84cbd907c9873939d0cb6562956304a8a57d4b9c
Emmanuel Peyronnet <azur.q3@gmail.com>
2019-07-17 23:39:44 +0200

    split functions to prepare for random script

--- Fractal/Simple/draw.py
+++ Fractal/Simple/draw.py
@@ -1,140 +1,13 @@
 import sys
 import os
-import colorsys
 import json
-from PIL import Image
-from PIL import ImageDraw
 import itertools
+from functions import to_list, list_of_list_transform, extract_poly, create_palette, iterate, generate_result_path, draw, write_inputs
 
-def main(folder_save):
+def draw(folder_save, display = False):
     image_number = 0
 
-
-    def to_list(x):
-        if not isinstance(x, list):
-            x = [x]
-        return(x)
-
-
-    def list_of_list_transform(list):
-        new_list = []
-        for degree in range(len(list)):
-            if len(new_list) == 0:
-                new_list = [[x] for x in list[degree]]
-            else:
-                temp_list = []
-                for value in list[degree]:
-                    temp_list = temp_list + [x + [value] for x in new_list]
-                new_list = temp_list
-        return(new_list)
-
-
-    def extract_poly(dict):
-        poly_list = []
-        for key in dict:
-            if len(poly_list) <= int(key):
-                poly_list = poly_list + [to_list(0) for _ in range(int(key) - len(poly_list) + 1)]
-            if "imaginary" in dict[key]:
-                if "real" in dict[key]:
-                    poly_list[int(key)] = [complex(a, b) for (a, b) in itertools.product(to_list(dict[key]["real"]), to_list(dict[key]["imaginary"]))]
-                else:
-                    poly_list[int(key)] = [complex(0, b) for b in to_list(dict[key]["imaginary"])]
-            else:
-                poly_list[int(key)] = [a for a in to_list(dict[key]["real"])]
-        return(poly_list)
-
-
-    def create_palette(r_start, r_coef, g_start, g_coef, b_start, b_coef, speed, dark2light, colors_max):
-        if (r_start < 0) or (r_start > 1):
-            print("r_start is ill-defined")
-            return(None)
-        if (g_start < 0) or (g_start > 1):
-            print("g_start is ill-defined")
-            return(None)
-        if (b_start < 0) or (b_start > 1):
-            print("b_start is ill-defined")
-            return(None)
-        if (r_start + r_coef < 0) or (r_start + r_coef > 1):
-            print("r_coef is ill-defined")
-            return(None)
-        if (g_start + g_coef < 0) or (g_start + g_coef > 1):
-            print("g_coef is ill-defined")
-            return(None)
-        if (b_start + b_coef < 0) or (b_start + b_coef > 1):
-            print("b_coef is ill-defined")
-            return(None)
-
-        palette = [0] * colors_max
-
-        for i in range(colors_max):
-            if dark2light:
-                f = 1 + (float(i) / colors_max - 1) ** speed
-            else:
-                f = - (float(i) / colors_max - 1) ** speed
-            r, g, b = colorsys.hsv_to_rgb(r_start + f * r_coef, g_start + f * g_coef, b_start + f * b_coef)
-
-            palette[i] = (int(r*255), int(g*255), int(b*255))
-
-        return(palette)
-
-
-    def iterate(z, poly, threshold, n_iterations):
-        p = len(poly)
-        for n in range(n_iterations + 1):
-            z = sum(poly[i] * (z ** i) for i in range(p))
-            if abs(z) > threshold:
-                return n
-        return None
-
-
-    def generate_result_path(folder_save):
-        path = "Results/"
-        input_path = path
-        if folder_save is not None:
-            path = path + str(folder_save) + "/"
-            input_path = input_path + str(folder_save) + "/"
-            if not os.path.exists(input_path):
-                os.makedirs(input_path)
-        return(path, input_path)
-
-
-    def draw(path, image_number, poly, n_iterations, dimensions, threshold, scaling_factor, right_shift, upward_shift, palette):
-
-        center = (scaling_factor / 2, (scaling_factor / 2) * dimensions[1]/dimensions[0])
-
-        img = Image.new("RGB", dimensions)
-        d = ImageDraw.Draw(img)
-
-        for x in range(dimensions[0]):
-            for y in range(dimensions[1]):
-                z = complex(x * scaling_factor / dimensions[0] - center[0] - right_shift, y * scaling_factor / dimensions[0] - center[1] + upward_shift)
-                n = iterate(z, poly, threshold, n_iterations)
-
-                if n is None:
-                    v = 1
-                else:
-                    v = n/float(n_iterations)
-
-                d.point((x, y), fill = palette[int(v * (colors_max - 1))])
-
-        img.save(path + str(image_number) + ".png")
-        del d
-
-
-    def write_inputs(input_path, poly, input_dict):
-        output_file = input_dict.copy()
-        for degree in range(len(poly)):
-            if poly[degree] != 0:
-                output_file[str(degree)] = {}
-                if poly[degree].real != 0:
-                    (output_file[str(degree)])["real"] = (poly[degree]).real
-                if poly[degree].imag != 0:
-                    (output_file[str(degree)])["imaginary"] = (poly[degree]).imag
-        with open(input_path + str(image_number) + "-inputs.json", 'w') as f:
-            json.dump(output_file, f)
-
-
-    with open('inputs.json') as json_file:
+    with open('draw-inputs.json') as json_file:
         inputs = json.load(json_file)[0]
 
 
@@ -170,14 +43,19 @@ def main(folder_save):
                 for poly in poly_list:
 
                     path, input_path = generate_result_path(folder_save)
-                    draw(path, image_number, poly, n_iterations, dimensions, threshold, scaling_factor, right_shift, upward_shift, palette)
-                    write_inputs(input_path, poly, input_dict)
+                    draw(path, image_number, poly, n_iterations, dimensions, threshold, scaling_factor, right_shift, upward_shift, palette, colors_max, display)
+                    write_inputs(input_path, poly, input_dict, image_number)
                     image_number += 1
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        main(None)
+        draw(None)
     else:
         folder_save = sys.argv[1]
-        main(folder_save)
+        if len(sys.argv) == 2:
+            draw(folder_save)
+        else:
+            display = sys.argv[2]
+            print(display)
+            draw(folder_save, display)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Drawings
 Making (Trying to) Art With Python
+
+# Todo
+
+- Split draw.py code as maximum
+- Comment code
+- Add rotation parameter for Drawings
+- Make random generator that takes draw-inputs as inputs and random-inputs that override it (poly and color especially)
