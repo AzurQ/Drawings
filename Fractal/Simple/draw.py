506700070f96c75d8823bb6f812ca1b54f34867a
Emmanuel Peyronnet <azur.q3@gmail.com>
2019-07-20 22:16:29 +0200

    create Input folder

--- Fractal/Simple/draw.py
+++ Fractal/Simple/draw.py
@@ -7,8 +7,8 @@ from functions import to_list, list_of_list_transform, extract_poly, create_pale
 
 # Draw a fractal from given inputs
 ## Inputs may be provided as lists - all possibilites are produced
-def draw(input_dict, folder_save, display = False, image_number = 0):
-    # output_dict (dict) is dictionnary with all inputs
+def draw(draw_input_dict, folder_save, display = False, image_number = 0):
+    # draw_input_dict (dict) is dictionnary with all inputs
     # folder_save (str) is path to folder where results should be saved
     # display (bool) indicates if plots should be prompted
     # image_number (int) is the starting number for the images to be saved (in their names)
@@ -16,10 +16,10 @@ def draw(input_dict, folder_save, display = False, image_number = 0):
     # Convert color options into lists to iterate on
     input_to_convert_list_1 = ["r_start", "r_coef", "g_start", "g_coef", "b_start", "b_coef", "speed", "dark2light", "colors_max"]
     for item in input_to_convert_list_1:
-        input_dict[item] = to_list(input_dict[item])
+        draw_input_dict[item] = to_list(draw_input_dict[item])
 
     # Iterate for color options in input file
-    for r_start, r_coef, g_start, g_coef, b_start, b_coef, speed, dark2light, colors_max in itertools.product(input_dict["r_start"], input_dict["r_coef"], input_dict["g_start"], input_dict["g_coef"], input_dict["b_start"], input_dict["b_coef"], input_dict["speed"], input_dict["dark2light"], input_dict["colors_max"]):
+    for r_start, r_coef, g_start, g_coef, b_start, b_coef, speed, dark2light, colors_max in itertools.product(draw_input_dict["r_start"], draw_input_dict["r_coef"], draw_input_dict["g_start"], draw_input_dict["g_coef"], draw_input_dict["b_start"], draw_input_dict["b_coef"], draw_input_dict["speed"], draw_input_dict["dark2light"], draw_input_dict["colors_max"]):
 
         # Create the graphical palette
         palette = create_palette(r_start, r_coef, g_start, g_coef, b_start, b_coef, speed, dark2light, colors_max)
@@ -31,14 +31,14 @@ def draw(input_dict, folder_save, display = False, image_number = 0):
         if palette is not None:
 
             # Convert graphical options into lists to iterate on
-            input_dict["dimensions"]["x"] = to_list(input_dict["dimensions"]["x"])
-            input_dict["dimensions"]["y"] = to_list(input_dict["dimensions"]["y"])
+            draw_input_dict["dimensions"]["x"] = to_list(draw_input_dict["dimensions"]["x"])
+            draw_input_dict["dimensions"]["y"] = to_list(draw_input_dict["dimensions"]["y"])
             input_to_convert_list_2 = ["n_iterations", "threshold", "scaling_factor", "right_shift", "upward_shift"]
             for item in input_to_convert_list_2:
-                input_dict[item] = to_list(input_dict[item])
+                draw_input_dict[item] = to_list(draw_input_dict[item])
 
             # Iterate for graphical options in input file
-            for n_iterations, threshold, scaling_factor, right_shift, upward_shift, dimension_x, dimension_y in itertools.product(input_dict["n_iterations"], input_dict["threshold"], input_dict["scaling_factor"], input_dict["right_shift"], input_dict["upward_shift"], input_dict["dimensions"]["x"], input_dict["dimensions"]["y"]):
+            for n_iterations, threshold, scaling_factor, right_shift, upward_shift, dimension_x, dimension_y in itertools.product(draw_input_dict["n_iterations"], draw_input_dict["threshold"], draw_input_dict["scaling_factor"], draw_input_dict["right_shift"], draw_input_dict["upward_shift"], draw_input_dict["dimensions"]["x"], draw_input_dict["dimensions"]["y"]):
 
                 # Update dict used for saving/writing specific input data
                 output_dict["n_iterations"] = n_iterations
@@ -50,7 +50,7 @@ def draw(input_dict, folder_save, display = False, image_number = 0):
                 dimensions = [dimension_x, dimension_y]
 
                 # Import the dictionnary of polynomial
-                poly_raw_list = extract_poly(input_dict["polynomial"])
+                poly_raw_list = extract_poly(draw_input_dict["polynomial"])
                 # Make the list of polynomials implicitely defined in the dictionnary
                 poly_list = list_of_list_transform(poly_raw_list)
 
@@ -76,15 +76,14 @@ if __name__ == "__main__":
                         help="Folder path for result saving (str)")
     parser.add_argument("-d", "--display", default=False, type = bool,
                         help="Display plots (bool)")
-    parser.add_argument("-i", "--input", default="draw-inputs.json", type = str,
+    parser.add_argument("-i", "--input", default="Inputs/draw-inputs.json", type = str,
                         help="Drawing input path (str)")
     parser.add_argument("-n", "--number", default=0,
                         help="Start image number (int)")
     args = parser.parse_args()
 
     # Load input file
-    draw_input_file = args.input if args.input else "draw-inputs.json"
-    with open(draw_input_file) as json_file:
-        input_dict = json.load(json_file)[0]
+    with open(args.input) as json_file:
+        draw_input_dict = json.load(json_file)[0]
 
-    draw(input_dict, args.folder, args.display, int(args.number))
+    draw(draw_input_dict, args.folder, args.display, int(args.number))
--- Fractal/Simple/random.py
+++ Fractal/Simple/random.py
@@ -7,11 +7,11 @@ from functions import draw
 # Draw a fractal from given inputs and random distribution for given inputs
 ## Fixed inputs may be provided as lists - all possibilites are produced
 ## Randomly generated inputs override general fixed inputs
-def random(folder_save, display = False, draw_input_file = "draw-inputs.json", random_input_file = "random-inputs.json", image_number = 0):
+def random(draw_input_dict, random_intput_dict, folder_save, display = False, image_number = 0):
+    # draw_input_dict (doct) is dictionnary with fixed parameters
+    # random_intput_dict (dict) is dictionnary with random parameter distributions
     # folder_save (str) is path to folder where results should be saved
     # display (bool) indicates if plots should be prompted
-    # draw_input_file (str) is path to input file containing fixed parameters
-    # random_input_file (str) is path to input file containing random parameter distributions
     # image_number (int) is the starting number for the images to be saved (in their names)
 
 
@@ -22,12 +22,20 @@ if __name__ == "__main__":
                         help="Folder path for result saving (str)")
     parser.add_argument("-d", "--display", default=False, type = bool,
                         help="Display plots (bool)")
-    parser.add_argument("-i", "--input", default="draw-inputs.json", type = str,
+    parser.add_argument("-i", "--input", default="Inputs/draw-inputs.json", type = str,
                         help="Drawing input path (str)")
-    parser.add_argument("-r", "--random", default="random-inputs.json", type = str,
+    parser.add_argument("-r", "--random", default="Inputs/random-inputs.json", type = str,
                         help="Random input path (str)")
     parser.add_argument("-n", "--number", default=0,
                         help="Start image number (int)")
     args = parser.parse_args()
 
-    random(args.folder, args.display, args.input, args.random, int(args.number))
+    # Load fixed input file
+    with open(args.input) as draw_json_file:
+        draw_input_dict = json.load(draw_json_file)[0]
+
+    # Load random input file
+    with open(args.random) as random_json_file:
+        random_intput_dict = json.load(random_json_file)[0]
+
+    random(draw_input_dict, random_intput_dict, args.folder, args.display, int(args.number))
